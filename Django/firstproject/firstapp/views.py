294da760cf4efafb83532c89787c00cd20673025
Morgan N. Andayi <143075077+andayi-ck@users.noreply.github.com>
2025-06-19 17:05:23 +0300

    Update views.py

--- Django/firstproject/firstapp/views.py
+++ Django/firstproject/firstapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
